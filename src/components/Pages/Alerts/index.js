6fffbda5fed3917af36d2b0c40c56bd3d28847d1
Mohammed <mohammed.ntambiye@stockport.gov.uk>
2018-05-01 11:29:40 +0100

    Mohammed/Simon/Jamie modifying the alert information and alert warning

--- src/components/Pages/Alerts/index.js
+++ src/components/Pages/Alerts/index.js
@@ -79,24 +79,24 @@ class Alerts extends Component {
 						<p>The icon is centre aligned. When there is only one line of text the dividers are 20px away from the icon. When there is multiple lines the dividers are 20px away from the text.</p>
 						<section>
 							<h3>Information alert</h3>
-							<section className="alert-form-information">
+							<section className="alert-form alert-form-information">
 								<i className="fa fa-info-circle"></i>
-								<p>I'm a sentence on <strong>one line</strong> within an in-form alert.</p>
+								<p className="content">I'm a sentence on <strong>one line</strong> within an in-form alert.</p>
 							</section>
-							<section className="alert-form-information">
+							<section className="alert-form alert-form-information">
 								<i className="fa fa-info-circle"></i>
-								<p>I'm some body text that gives the user some additional information about a question or an answer in a form.</p>
+								<p className="content">I'm some body text that gives the user some additional information about a question or an answer in a form.</p>
 							</section>
 						</section>
 						<section>
 							<h3>Warning alerts</h3>
-							<section className="alert-form-warning">
-								<i className="fa fa-info-exclamation"></i>
+							<section className="alert-form alert-form-warning">
+								<i className="fa fa-info-circle"></i>
 								<p>I'm some body text that gives the user a warning message about a question or an answer in a form. This example shows what the alert should look like when it goes onto three lines.</p>
 							</section>
-							<section className="alert-form-warning">
+							<section className="alert-form alert-form-warning">
 								<i className="fa fa-info-circle"></i>
-								<p>I'm some body text that warns the user of something in a form.</p>
+								<p className='content'>The sort code and account number don’t match. Check your details and try again</p>
 							</section>
 						</section>
 					</section>
