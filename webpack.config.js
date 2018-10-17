5b92679b8ba9ec7273ca84797070d47c2bb9c238
Colin Lees <colin.lees@stockport.gov.uk>
2018-10-17 11:16:37 +0100

    fix(npm audit): Fixing breaking changes within npm audit logs

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,86 @@
 const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const paths = {
-    BUILD: path.resolve(__dirname, 'build'),
-    SRC: path.resolve(__dirname, 'src'),
-    CSS: path.resolve(__dirname, 'css')
-}
+module.exports = (env, argv) => {
+	config = {
+		entry: ['babel-polyfill', './src/index.js'],
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'app.bundle.js',
+			publicPath: '/'
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './src/index.html'
+			}),
+			new MiniCssExtractPlugin({
+				filename: "styles.min.css"
+			}),
+			new CleanWebpackPlugin('./dist'),
+			new CopyWebpackPlugin([
+				{ from: 'src/web.config', to: '/dist' }
+			]),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: [
+						'babel-loader',
+						'eslint-loader'
+					]
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						argv === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+						"css-loader",
+						"sass-loader"
+					]
+				},
+				{
+					test: /\.(eot|svg|ttf|woff|ico)$/,
+					use: [{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]'
+						}
+					}],
+				}
+			]
+		}
+	}
 
-module.exports = {
-    entry: {
-        main: [
-            'babel-polyfill',
-            path.join(paths.SRC, 'index.js'),
-        ]
-    },
-    output: {
-        path: paths.BUILD,
-        filename: 'app.bundle.js',
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(paths.SRC, 'index.html'),
-        }),
-        new ExtractTextPlugin('styles.min.css'),
-        new CleanWebpackPlugin([paths.BUILD]),
-        new CopyWebpackPlugin([
-            { from: 'src/web.config', to: paths.BUILD }
-        ]),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                    'eslint-loader'
-                ]
-            },
-            {
-                test: /\.(css|scss)$/,
-                exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: 'css-loader',
-                        options: { minimize: true }
-                    },
-						'sass-loader']
-                })
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|ico)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]'
-                    }
-                }],
-			}
-        ]
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
+	if (argv.mode === 'development') {
+		config.devtool = 'source-map'
+		config.devServer = {
+			inline: true,
+			historyApiFallback: true
+		}
+	}
+
+	if (argv.mode === 'production') {
+		config.optimization = {
+			splitChunks: {
+				cacheGroups: {
+					styles: {
+						name: 'styles',
+						test: /\.css$/,
+						chunks: 'all',
+						enforce: true
+					}
+				}
+			},
+			minimizer: [
+				new OptimizeCSSAssetsPlugin({})
+			]
+		}
+	}
+
+	return config
 }
