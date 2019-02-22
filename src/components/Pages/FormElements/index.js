20485e0ba481395c107b13886779dae07383e828
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-02-22 08:57:30 +0000

    Feature(Appointment selector): added appointment selector to stories

--- src/components/Pages/FormElements/index.js
+++ src/components/Pages/FormElements/index.js
@@ -298,6 +298,19 @@ class Inputs extends Component {
 								<button disabled="" id="submitFiles" type="submit" className="button-primary">Submit your documents</button>
 							</form>
 						</section>
+						<section>
+							<form>
+								<h1>Booking Selection</h1>
+								<h2>Choose an appointment</h2>
+								<h3>Wednesday 27 January 2019</h3>
+								<button className="button-inverted inline" href="#">08:45am</button>
+								<button className="button-inverted inline" href="#">12:45pm</button>
+								<h3>Wednesday 6 February 2019</h3>
+								<button className="button-inverted inline" href="#">08:45am</button>
+								<a className='anchor-with-icon' href='#'><i className="fa fa-plus-square fa-2x"></i>Show more dates</a>
+								<button className="button-primary">Next step</button>
+							</form>
+						</section>
 					</section>
 				</article>
 				<Aside />
