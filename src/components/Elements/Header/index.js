42702471594882ca3aaf4fd08407c9a28ae8e234
Colin Lees <colin.lees@stockport.gov.uk>
2019-07-12 09:47:17 +0100

    feat(MyAccountSearch): Amended class name of search-mobile

--- src/components/Elements/Header/index.js
+++ src/components/Elements/Header/index.js
@@ -34,8 +34,8 @@ class Header extends Component {
 					<div id="header-logo">
 						<a href="http://www.stockport.gov.uk">Stockport Metropolitan Borough Council Homepage</a>
 					</div>
-					<input id={uniqueKey ? `search-mobile-${uniqueKey}` : 'search-mobile'} type='checkbox' name='search-mobile' tabIndex='1' />
-					<label className='search-mobile-label' htmlFor={uniqueKey ? `search-mobile-${uniqueKey}` : 'search-mobile'}>
+					<input id={uniqueKey ? `search-mobile-checkbox-${uniqueKey}` : 'search-mobile-checkbox'} type='checkbox' name='search-mobile-checkbox' tabIndex='1' />
+                    <label className='search-mobile-label' htmlFor={uniqueKey ? `search-mobile-checkbox-${uniqueKey}` : 'search-mobile-checkbox'}>
 						<i className="fa fa-search" aria-hidden="true"></i>
 						<span className='hidden'>Open search bar</span>
 					</label>
